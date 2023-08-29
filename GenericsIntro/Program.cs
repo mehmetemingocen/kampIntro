38cff2af1b40a1b30ee540304247514893c424a4
Mehmet Emin Gocen <mehmetemingocen188@gmail.com>
2023-08-29 22:33:47 +0300

    4.gün ders sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,10 @@
+﻿using GenericsIntro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<string> isimler = new MyList<string>();
+        isimler.Add("Ahmet");
+    }
+}
